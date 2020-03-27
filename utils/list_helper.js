9b7900d99bfd2a60428383d62f4693c19ce0cd6b
ElMayo <mic.l.meier@gmail.com>
2020-03-27 21:11:14 +0100

    exercise helper functions and unit tests - step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = blogs => {
   return 1;
 };
 
+const totalLikes = blogs =>
+  blogs
+    .map(blog => blog.likes)
+    .reduce((accumulator, blog) => accumulator + blog, 0);
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
