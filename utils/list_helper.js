6148dce833856a01692dc4355ed84d2e51b60d80
ElMayo <mic.l.meier@gmail.com>
2020-03-28 07:33:45 +0100

    exercise helper functions and unit tests - step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,25 @@ const totalLikes = blogs =>
     .map(blog => blog.likes)
     .reduce((accumulator, blog) => accumulator + blog, 0);
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 1) {
+    return {
+      title: blogs[0].title,
+      author: blogs[0].author,
+      likes: blogs[0].likes
+    };
+  } else if (blogs.length > 1) {
+    return blogs.reduce((acc, blog) =>
+      blog.likes > acc.likes
+        ? { title: blog.title, author: blog.author, likes: blog.likes }
+        : acc
+    );
+  }
+  return undefined;
+};
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 };
