f612f164bb5079310c989ed96313808aea406209
ElMayo <mic.l.meier@gmail.com>
2020-03-29 10:56:40 +0200

    exercise blog list expansions - step1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -98,6 +98,17 @@ test("returns status code 400 if blog url is missing from POST request", async (
     .expect(400);
 });
 
+test("deletion of a blog succeeds with status code 204 if id is valid", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  expect(blogsAtEnd.length).toBe(helper.initialBlogs.length - 1);
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
