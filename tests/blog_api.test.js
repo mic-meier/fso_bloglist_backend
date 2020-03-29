47be6a1bb4339539bcd3b703b608b177a2c4e329
ElMayo <mic.l.meier@gmail.com>
2020-03-29 09:44:30 +0200

    exercise blog list tests - step3

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -35,6 +35,28 @@ test("unique identifier of blog posts is named id and not _id", async () => {
   });
 });
 
+test("POST request to endpoint /api/blogs with valid blog post successfully creates a new blog", async () => {
+  const newBlog = {
+    title: "What Are the React Team Principles?",
+    author: "Dan Abramov",
+    url: "https://overreacted.io/what-are-the-react-team-principles/",
+    likes: 42
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const res = await api.get("/api/blogs");
+
+  const titles = res.body.map(blog => blog.title);
+
+  expect(res.body.length).toBe(helper.initialBlogs.length + 1);
+  expect(titles).toContain("What Are the React Team Principles?");
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
