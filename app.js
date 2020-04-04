b851f3460e6d9b4c999c915d07f19a7f35dc04f9
ElMayo <mic.l.meier@gmail.com>
2020-04-04 09:29:19 +0200

    exercise bloglist expansion - step4

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -16,5 +17,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
